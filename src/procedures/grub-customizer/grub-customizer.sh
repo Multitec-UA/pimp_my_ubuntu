6f6d357d5d560d2c56d766790f9cfecc2007ad6a
Sergio Conejero <sergio.conejero@multitecua.com>
2025-03-26 17:41:06 +0100

    WIP refactor URLs

--- src/procedures/grub-customizer/grub-customizer.sh
+++ src/procedures/grub-customizer/grub-customizer.sh
@@ -28,7 +28,7 @@ readonly _LIBS_REMOTE_URL="${_REPOSITORY_RAW_URL}/src/libs/"
 # More themes in https://www.gnome-look.org/ Add .zip with theme.txt file inside to media folder
 readonly _THEME_OPTIONS=("crt-amber-theme" "monterey-theme" "solarized-theme" "cybergrub-theme") # Available theme options
 readonly _THEME_POSITION=${1:-0} #Default theme position, can be overridden by command line argument
-readonly _THEME_NAME="${_THEME_OPTIONS[$_THEME_POSITION]}"
+readonly _THEME_NAME="${_THEME_OPTIONS[_THEME_POSITION]}"
 readonly _MEDIA_PATH="/src/procedures/grub_customizer/media"
 
 # Declare GLOBAL_INSTALLATION_STATUS if not already declared
